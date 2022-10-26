4f152c24ad5753e4216d749382d05355367cc62c
marekvks <mara.vyky@gmail.com>
2022-10-26 20:39:24 +0200

    Comments

--- main.R
+++ main.R
@@ -6,7 +6,7 @@ sum_schools_county_df <- school_dataset %>%
   select(kraj) %>%
   count(kraj, name = "school_total", sort = TRUE)
 
-# Getting NA county
+# Getting the NA county
 na_county_df <- school_dataset %>%
   filter(is.na(kraj == TRUE))
 
@@ -15,43 +15,54 @@ student_per_county_df <- school_dataset %>%
   group_by(kraj) %>%
   summarize(student_count = sum(pocet_zaku))
 
-# Calculating the sum of students in nuts2
+# Calculating the sum of students in NUTS2
 student_per_nuts2_df <- nuts2_df %>%
   group_by(nuts2) %>%
   summarize(student_count = sum(pocet_zaku))
 
+# Calculating students per assistant by counties
 student_per_assistant_by_county_df <- nuts2_df %>%
   group_by(kraj) %>%
   summarize(student_count = sum(pocet_zaku),
             assistant_count = sum(pocet_asistentu)) %>%
   mutate(student_per_assistant = student_count / assistant_count)
 
+# Calculating students per assistant by NUTS2
 student_per_assistant_by_nuts2_df <- nuts2_df %>%
   group_by(nuts2) %>%
   summarize(student_count = sum(pocet_zaku),
             assistant_count = sum(pocet_asistentu)) %>%
   mutate(student_per_assistant = student_count / assistant_count)
 
+# Calculating average number of students in countie which doesnt have a psycholog
 average_students_without_psycholog <- school_dataset %>%
   filter(psycholog == "ne") %>%
   summarize(avg_students_without_psycholog = mean(pocet_zaku))
 
+# Calculating average number of students in countie which has a psycholog
 average_students_with_without_psycholog <- school_dataset %>%
   filter(psycholog == "ano") %>%
   summarize(avg_students_with_psycholog = mean(pocet_zaku))
 
-average_students_without_psycholog <- school_dataset %>%
+# Calculating average number of students in countie which doesnt have a special pedagog
+average_students_without_spec_pedagog <- school_dataset %>%
   filter(spec_pedagog == "ne") %>%
   summarize(avg_students_without_sp_pedagog = mean(pocet_zaku))
 
-average_students_with_without_psycholog <- school_dataset %>%
+# Calculating average number of students in countie which has a psycholog
+average_students_with_without_spec_pedagog <- school_dataset %>%
   filter(spec_pedagog == "ano") %>%
   summarize(avg_students_with_sp_pedagog = mean(pocet_zaku))
 
+# Calculating sum of schools in each county with psycholog, special pedagog or both
 school_with_psycholog_pedagog <- school_dataset %>%
   filter(psycholog == "ano" | spec_pedagog == "ano") %>%
   count(kraj, name = "school_with_ped_or_psych", sort = TRUE)
 
-school_with_psycholog_pedagog_percentage <- left_join(sum_schools_county_df, school_with_psycholog_pedagog) %>%
+# Calculating percentage of schools which have psycholog, spec_pedagog or both
+school_with_psycholog_pedagog_percentage_df <- left_join(sum_schools_county_df, school_with_psycholog_pedagog) %>%
   group_by(kraj) %>%
   mutate(percentage = school_with_ped_or_psych / (school_total / 100))
+
+# Remove unnecessary data frames
+rm(school_with_psycholog_pedagog)
